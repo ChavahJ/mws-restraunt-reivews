fbd97a0bc45eb815bb3ba7bf17e18871c3751f00
Chavah <elynchav@gmail.com>
2018-06-19 23:55:11 +0300

    06192018 adding service worker

--- js/main.js
+++ js/main.js
@@ -197,3 +197,15 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     }
   });
 };
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
